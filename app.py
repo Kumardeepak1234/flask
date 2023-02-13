8cd8968df270aec685ced7dd197a9dbffff8ce81
Deepak kumar <kumardeepak11042@gmail.comC:/Users/D/AppData/Local/Programs/Python/Python310/python.exe>
2023-02-13 19:51:59 +0530

    UPDATE THIS FILE

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-from flask import Flask
+from flask import Flask,request
 app=Flask(__name__)
 #Router 
 @app.route("/")
-def hello_world():
+def welcome():
     return "Hello word"
+@app.route("/aboutus")
+def aboutus():
+    return "we are ineuron"
+@app.route('/demo',methods=['POST'])
+def math_operation():
+    if(request.methods=='POST'):
+        operation=request.json['operation']
+        num1=request.json['num1']
+        num2=request.json['num2']
+        result=num1+num2
+        return "the operation is {} and the result is {}".format(operation,result)
+
 if __name__=="__main__":
     app.run(host="0.0.0.0",port=5000)
